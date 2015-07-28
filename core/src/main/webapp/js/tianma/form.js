f43318c059c6a1ccd62f336bd00807cc6c127865
wmx2003 <wmx2003@aliyun.com>
2015-07-28 16:22:08 +0800

    aa

--- core/src/main/webapp/js/tianma/form.js
+++ core/src/main/webapp/js/tianma/form.js
@@ -37,9 +37,15 @@ function newInputItem(options){
 }
 
 function newInputElement(options){
-	var newOpt = copyOptions(options,["label"]);
-
-	var htmlType = newOpt.type;
+	var htmlType = options.type;
+	if(htmlType === "list"){
+		return newListEditor(options);
+	}
+	
+	var newOpt = eliminate(options,["label"]);
+	if(newOpt.prefix && newOpt.name){
+		newOpt.name = newOpt.prefix + '.' + newOpt.name;
+	}
 	if(htmlType == "select"){
 		return newHtmlSelect(newOpt);
 	}else if(htmlType == "html"){
@@ -75,7 +81,7 @@ function newHtmlSelect(options){
 	return selObj;
 }
 
-function copyOptions(map,excls){
+function eliminate(map,excls){
 	var newMap = {};
 	$.each(map,function(k,v){
 		for(idx in excls){
@@ -90,41 +96,168 @@ function copyOptions(map,excls){
 	return newMap;
 }
 
+/**
+ * List Editor
+ * **/
+function newListEditor(itemData){
+	var divObj = $("<div></div>").addClass("ul-list");
+
+	var options = initOptions(itemData);
+	addNewButton(options,divObj);
+	newListHeader(options.header).appendTo(divObj);
+	
+	if(itemData.value){
+		$.each(itemData.value,function(k,item){
+			newListItem(item,options).appendTo(divObj);
+		});
+	}
+	
+	newInputElement(options.maxIndex).appendTo(divObj);
+	
+	return divObj;
+};
+
+function newListItem(inputItem,options){
+	var ulObj = $("<ul></ul>").attr("row",options.maxIndex.value);
+	
+	var col = 0;
+	if(options.header){
+		$.each(options.header,function(idx){
+			var fieldInput = inputItem[idx];
+		
+			var liObj = $("<li></li>").attr("column",col++).appendTo(ulObj);
+			if(fieldInput instanceof Array){
+				$.each(fieldInput,function(k,opt){
+					if(!opt)return true;
+					
+					newItemInput(opt,options).appendTo(liObj);
+				});
+				//options.emptyItem[idx] = fieldInput;
+			}else if(fieldInput){
+				newItemInput(fieldInput,options).appendTo(liObj);
+				//options.emptyItem[idx] = fieldInput;
+			}
+		});
+	}
+
+	addItemDeleteButton(ulObj,col);
+	options.maxIndex.value++;
+	
+	return ulObj;
+};
+
+function newItemInput(itemOpt,options){
+	var oldName = itemOpt.name;
+	itemOpt.name = options.maxIndex.value+'.'+itemOpt.name;
+	if(!itemOpt.prefix)itemOpt.prefix = options.prefix;
+	
+	var inputObj = newInputElement(itemOpt).css("margin-left","5px");
+	
+	itemOpt.name = oldName;
+	itemOpt.value = null;
+	
+	return inputObj;
+}
+
+function initOptions(items){
+	return {
+		label:items.label,
+		header:items["item-labels"],
+		prefix:items["item-prefix"],
+		emptyItem:items.template,
+		maxIndex:{
+			value:0,
+			type:'hidden',
+			prefix:items["item-prefix"],
+			name:'maxIndex'
+		}
+	};
+}
+
+function addNewButton(options,divObj){
+	return $("<input type='button' id='addNewItemRow' />").val('添加'+options.label)
+			.click(function(){
+				newListItem(options.emptyItem,options).appendTo(divObj);
+			}).appendTo($("<li />").appendTo($("<ul />").addClass("operator").appendTo(divObj)));
+}
+
+function newListHeader(header){
+	var itemHeaders = [];
+	$.each(header,function(key,label){
+		itemHeaders.push({'name':key,'label':label});
+	});
+	itemHeaders.push({'name':'operator','label':'操作'});
+	
+	var ulObj = $("<ul></ul>").addClass("header");
+	var col = 0;
+	if(itemHeaders){
+		$.each(itemHeaders,function(key,value){
+			if(value){
+				$("<li></li>").attr("column",col++).text(value.label).appendTo(ulObj);
+			}
+		});
+	}
+	
+	return ulObj;
+};
+
+function addItemDeleteButton(ulObj,col){
+	return $("<input type='button' value='删除' />").click(function(){
+			ulObj.remove();
+		}).appendTo($("<li></li>").attr("column",col).appendTo(ulObj));
+}
 
 /*
-function newInputElement(inputData){
-		if(inputData.type == "text"){
-			return TM_htmlBuilder.newText(inputData);
-		}else if(inputData.type == "select"){
-			return TM_htmlBuilder.newSelect(inputData);
-		}else if(inputData.type == "hidden"){
-			return TM_htmlBuilder.newHiddenText(inputData);
-		}else if(inputData.type == "date"){
-			return TM_htmlBuilder.newDateInput(inputData);
-		}else if(inputData.type == "number"){
-			return TM_htmlBuilder.newNumberInput(inputData);
-		}else if(inputData.type == "picture"){
-			return TM_htmlBuilder.newFileText(inputData);
-		}else if(inputData.type == "file"){
-			return TM_htmlBuilder.newFileText(inputData);
-		}else if(inputData.type == "radio"){
-			return TM_htmlBuilder.newRadio(inputData);
-		}else if(inputData.type == "checkbox"){
-			return TM_htmlBuilder.newCheckbox(inputData);
-		}else if(inputData.type == "textarea"){
-			return TM_htmlBuilder.newTextarea(inputData);
-		}else if(inputData.type == "label"){
-			return builder.newLabel(inputData);
-		}else if(inputData.type == "between"){
-			var spanObj = $("<span></span>");
-			var minObj = builder.newInputElement(inputData.min);
-			var maxObj = builder.newInputElement(inputData.max);
-			var speObj = $("<label>-</label>");
-			
-			spanObj.append(minObj).append(speObj).append(maxObj);
-			
-			return spanObj;
-		}else if(inputData.type == "list"){
-			return TM_ulListBuilder.newInputs(inputData);
+function UIListBuilder(){
+	var builder = this;
+	
+	this.newUIList = function(listData){
+		var divObj = $("<div></div>").addClass("ul-list");
+		
+		divObj.append(builder.newListHeader(listData.header));
+
+		var uls = builder.newListData(listData);
+		for(var idx in uls){
+			divObj.append(uls[idx]);
 		}
-	};*/
+		return divObj;
+	};
+	
+	this.newListData = function(listData){
+		var headerIndex = [];
+		var idx = 0;
+		for(var i in listData.header){
+			headerIndex[idx++] = listData.header[i].name;
+		}
+		
+		var ulAry = [];
+		idx = 0;
+		for(var i in listData.data){
+			var modelData = listData.data[i];
+			
+			var ulObj = $("<ul></ul>");
+			for(var ii in headerIndex){
+				var key = headerIndex[ii];
+				var htmlData = modelData[key];
+				
+				var liObj = $("<li></li>");
+				var htmlObj;
+				if(htmlData instanceof Array){
+					for(var iii in htmlData){
+						htmlObj = TM_htmlBuilder.newHtmlElement(htmlData[iii]);
+						liObj.append(htmlObj);
+					}
+				}else {
+					htmlObj = TM_htmlBuilder.newHtmlElement(htmlData);
+					liObj.append(htmlObj);
+				}
+				
+				ulObj.append(liObj);
+			}
+			
+			ulAry[idx++] = ulObj;
+		}
+		
+		return ulAry;
+	};
+}*/
