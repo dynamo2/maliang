8bd4c95664a60eacf2171c5a1de6d4b1b637df48
dynamo2 <dynamo2.miles@aliyun.com>
2015-08-07 22:55:44 +0800

    Add docs.

---
+++ core/doc/General Element in Design Thinking.mm
@@ -0,0 +1,7 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1436108635713" ID="ID_1496865813" MODIFIED="1436108705831" TEXT="General Element in Design Thinking">
+<node CREATED="1436108653811" ID="ID_1453496910" MODIFIED="1436108658986" POSITION="right" TEXT="How to troubleshooting"/>
+<node CREATED="1436108718303" ID="ID_1698878899" MODIFIED="1436108742785" POSITION="right" TEXT="How to log the system behavior"/>
+</node>
+</map>
