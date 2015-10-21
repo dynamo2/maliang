0688c379eb9cffd4e4a975f191a9eec10adbee82
wmx2003 <wmx2003@aliyun.com>
2015-10-21 09:49:26 +0800

    aa

---
+++ core/src/main/java/com/maliang/core/arithmetic/function/QueryFunction.java
@@ -0,0 +1,75 @@
+package com.maliang.core.arithmetic.function;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import com.maliang.core.arithmetic.ArithmeticExpression;
+
+public class QueryFunction {
+	public static void main(String[] args) {
+		String str = "[{name:'wmx',age:20,grade:{id:2,name:'普通会员'}},"
+				+ "{name:'wf',age:30,grade:{id:2,name:'普通会员'}},"
+				+ "{name:'wzq',age:4,grade:{id:4,name:'高级会员'}},"
+				+ "{name:'wzq',age:5,grade:{id:8,name:'高级会员'}}].query(((age>10 & name='wmx') | grade.id=8 | (age=grade.id))).max(grade.id)";
+		
+		Object ov = ArithmeticExpression.execute(str,null);
+		System.out.println(ov);
+		
+		str = "[1,2,3,4,5].max()";
+		ov = ArithmeticExpression.execute(str,null);
+		System.out.println("ov : " + ov);
+	}
+	
+	private static boolean isArray(Object ob){
+		if(ob != null && ((ob instanceof Collection) || (ob instanceof Object[]))){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private static boolean isMap(Object ob){
+		if(ob != null && ob instanceof Map){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static Object execute(Function function,Map<String,Object> params){
+		Object operatedObj = function.getKeyValue();
+		
+		if(isArray(operatedObj)){
+			if(operatedObj instanceof Collection){
+				operatedObj = ((Collection)operatedObj).toArray();
+			}
+
+			List<Object> resultList = new ArrayList<Object>();
+			Object[] dataList = (Object[])operatedObj;
+			for(Object obj : dataList){
+				if(!(obj instanceof Map)){
+					continue;
+				}
+				
+				Object ov = ArithmeticExpression.execute(function.expression, (Map)obj);
+				if(ov instanceof Boolean && (Boolean)ov){
+					resultList.add(obj);
+				}
+			}
+			
+			return resultList;
+		}
+		
+		if(isMap(operatedObj)){
+			Object ov = ArithmeticExpression.execute(function.expression, (Map)operatedObj);
+			if(ov instanceof Boolean && (Boolean)ov){
+				return operatedObj;
+			}
+			return null;
+		}
+
+		return null;
+	}
+}
