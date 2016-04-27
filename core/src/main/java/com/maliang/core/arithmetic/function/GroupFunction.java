b34ca290f7bf727d77385e4b84f6757b921f751d
wmx2003 <wmx2003@aliyun.com>
2016-04-27 10:55:04 +0800

    aa

--- core/src/main/java/com/maliang/core/arithmetic/function/GroupFunction.java
+++ core/src/main/java/com/maliang/core/arithmetic/function/GroupFunction.java
@@ -177,46 +177,4 @@ public class GroupFunction {
 		
 	}
 	
-//	public static Object[] expand(Object[] expandList,String[] names){
-//		if(Utils.isEmpty(expandList))return expandList;
-//		if(Utils.isEmpty(names))return expandList;
-//		
-//		int end = 0;
-//		List<Object> newDatas = new ArrayList<Object>();
-//		for(Object obj:expandList){
-//			boolean isExpand = false;
-//			List<String> expands = new ArrayList<String>();
-//			end = 0;
-//			Object parent = obj;
-//			for(String n: names){
-//				expands.add(n);
-//				Object val = MapHelper.readValue(parent,n);
-//				if(val instanceof List){
-//					for(Object listObj : (List)val){
-//						Object nd = Utils.clone(obj);
-//						MapHelper.setValue(nd,expands,listObj);
-//						newDatas.add(nd);
-//					}
-//
-//					isExpand = true;
-//					break;
-//				}
-//				
-//				end++;
-//				parent = val;
-//			}
-//			if(!isExpand){
-//				newDatas.add(obj);
-//			}
-//		}
-//		
-//		Object[] results = newDatas.toArray();
-//		if(end < names.length){
-//			results = expand(results,names);
-//		}
-//		
-//		return results;
-//	}
-	
-	
 }
