95f536e849b1a32682672d9d3a276d2fb04d56ab
wmx2003 <wmx2003@aliyun.com>
2016-07-25 14:54:13 +0800

    aa

--- core/src/main/java/com/maliang/core/arithmetic/AE.java
+++ core/src/main/java/com/maliang/core/arithmetic/AE.java
@@ -1,6 +1,12 @@
 package com.maliang.core.arithmetic;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.maliang.core.service.BusinessService;
 
 public class AE {
 	public static Object execute(String expre){
@@ -10,4 +16,12 @@ public class AE {
 	public static Object execute(String expre,Map<String,Object> params){
 		return ArithmeticExpression.execute(expre,params);
 	}
+	
+	public static void main(String[] args) {
+		String s = "{gfdsgfdgds${AAA.BBB} rtrewt } fdsafdsa ${ CCCC.DDDD } }";
+		
+		System.out.println((new BusinessService()).readBlock(s));
+	}
 }
+
+
--- core/src/main/java/com/maliang/core/service/BusinessService.java
+++ core/src/main/java/com/maliang/core/service/BusinessService.java
@@ -1,6 +1,10 @@
 package com.maliang.core.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.maliang.core.arithmetic.ArithmeticExpression;
 import com.maliang.core.dao.BusinessDao;
@@ -28,5 +32,67 @@ public class BusinessService {
 		ArithmeticExpression.execute(flow.getCode(), params);
 		
 		return ArithmeticExpression.execute(flow.getResponse(), params);
+	}
+	
+	public String readBlock(String code) {
+		String startRgx = "\\$\\{";
+		String endRgx = "\\}";
+
+		int startIndex = 0;
+		
+		Pattern pStart = Pattern.compile(startRgx);
+		Pattern pEnd = Pattern.compile(endRgx);
+		
+		Matcher mStart = pStart.matcher(code);
+		Matcher mEnd = pEnd.matcher(code);
+		
+		List<BlockNode> list = new ArrayList<BlockNode>();
+		while(mStart.find(startIndex)){
+			mEnd.find(mStart.start()+2);
+			
+			BlockNode node = new BlockNode(code,mStart.start(),mEnd.start());
+			list.add(node);
+			
+			startIndex = mEnd.end();
+		}
+		
+		StringBuffer sbf = new StringBuffer(code);
+		for(BlockNode n : list){
+			n.replace(sbf,"<TEST333>");
+		}
+		return sbf.toString();
+	}
+}
+
+class BlockNode {
+	final String source;
+	final int start;
+	final int end;
+	String label;
+	
+	BlockNode(String s,int st,int e){
+		this.source = s;
+		this.start = st;
+		this.end = e;
+		
+		this.read();
+	}
+	
+	String getLabel(){
+		return this.label;
+	}
+	
+	StringBuffer replace(StringBuffer sbf,String newStr){
+		if(sbf == null){
+			sbf = new StringBuffer(source);
+		}
+		return sbf.replace(start, end,newStr);
+	}
+	
+	private void read(){
+		this.label = this.source.substring(start+2,end);
+		if(!label.isEmpty()){
+			this.label = this.label.trim();
+		}
 	}
 }
